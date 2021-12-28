9773ee0d5a5b668413f375f089fcea574e1d7dd1
Nuthasith G <nuthasith1997@gmail.com>
2021-12-28 20:37:05 +0700

    init

---
+++ zltech_driver_example.ino
@@ -0,0 +1,127 @@
+#include <ModbusMaster.h>
+#include <SoftwareSerial.h>
+// https://github.com/4-20ma/ModbusMaster
+// https://github.com/PaulStoffregen/SoftwareSerial
+
+#include "zltech_item.h"
+
+int32_t position[2] = {0, 0};
+int16_t velocity[2] = {0, 0};
+uint16_t acc_time = 50;
+uint16_t dec_time = 50;
+
+ModbusMaster node;
+SoftwareSerial mySerial(6,7);       // pin 6 = RX, pin 7 = TX
+
+float Data_Modbus[5];
+
+void setup(){
+    Serial.begin(57600);
+    mySerial.begin(9600);
+    node.begin(1, mySerial);
+
+    Serial.println("Initializing..");
+
+    if (!setControlMode(3)){
+        Serial.println("Cannot set Velocity Mode.");
+    }
+    if (!setControlWord(8)){
+        Serial.println("Cannot enable motors.");
+    }
+    if (!setAccTime(acc_time)){
+        Serial.println("Cannot set AccTime.");
+    }
+    if (!setDecTime(dec_time)){
+        Serial.println("Cannot set DecTime.");
+    }
+}
+
+void loop(){
+    if (!getVelocity(velocity)){
+        Serial.println("Cannot get velocity.");
+    }
+    if (!getPosition(position)){
+        Serial.println("Cannot get position.");
+    }
+    Serial.print("Left = ");
+    Serial.print(velocity[0]);
+    Serial.print(" ");
+    Serial.print("Right = ");
+    Serial.print(velocity[1]);
+    Serial.print(" ");
+    Serial.print("PulseL = ");
+    Serial.print(position[0]);
+    Serial.print(" ");
+    Serial.print("PulseR = ");
+    Serial.println(position[1]);
+
+    setVelocity(0,0);
+}
+
+bool setControlMode(uint16_t value){
+    uint8_t result = node.writeSingleRegister(CONTROL_MODE, value);
+    return result == node.ku8MBSuccess;
+}
+
+bool setControlWord(uint16_t value){
+    uint8_t result = node.writeSingleRegister(CONTROL_WORD, value);
+    return result == node.ku8MBSuccess;
+}
+
+bool setAccTime(uint16_t value){
+    uint8_t result1 = node.writeSingleRegister(ACC_TIME_LEFT, value);
+    bool ret1 = (result1 == node.ku8MBSuccess);
+    uint8_t result2 = node.writeSingleRegister(ACC_TIME_RIGHT, value);
+    bool ret2 = (result2 == node.ku8MBSuccess);
+    return ret1 && ret2;
+}
+
+bool setDecTime(uint16_t value){
+    uint8_t result1 = node.writeSingleRegister(DECEL_TIME_LEFT, value);
+    bool ret1 = (result1 == node.ku8MBSuccess);
+    uint8_t result2 = node.writeSingleRegister(DECEL_TIME_RIGHT, value);
+    bool ret2 = (result2 == node.ku8MBSuccess);
+    return ret1 && ret2;
+}
+
+bool setVelocity(int16_t value_left, int16_t value_right){
+    uint8_t result1 = node.writeSingleRegister(TARGET_VEL_LEFT, value_left);
+    bool ret1 = (result1 == node.ku8MBSuccess);
+    uint8_t result2 = node.writeSingleRegister(TARGET_VEL_RIGHT, value_right);
+    bool ret2 = (result2 == node.ku8MBSuccess);
+    return ret1 && ret2;
+}
+
+bool getPosition(int32_t* dest){
+    uint16_t data[4];
+    uint8_t result = node.readHoldingRegisters(ACTUAL_POS_HIGH_LEFT, 4);
+    if (result == node.ku8MBSuccess){
+        for (uint8_t i = 0; i < 4; i++){
+            data[i] = node.getResponseBuffer(i);
+        }
+        *dest = data[0] << 16;
+        *dest = *dest + data[1];
+        *(dest+1) = data[2] << 16;
+        *(dest+1) = *(dest+1) + data[3];
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool getVelocity(int16_t* dest){
+    uint16_t data[2];    
+    uint8_t result = node.readHoldingRegisters(ACTUAL_VEL_LEFT, 2);
+    if (result == node.ku8MBSuccess){
+        for (uint8_t i = 0; i < 2; i++){
+            data[i] = node.getResponseBuffer(i);
+        }
+        *dest = data[0];
+        *(dest+1) = data[1];
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ zltech_item.h
@@ -0,0 +1,30 @@
+// Addresses Dictionary
+
+#define CONTROL_MODE            0x200D
+#define CONTROL_WORD            0x200E
+
+#define ACC_TIME_LEFT           0x2080
+#define ACC_TIME_RIGHT          0x2081
+#define DECEL_TIME_LEFT         0x2082
+#define DECEL_TIME_RIGHT        0x2083
+#define TORQUE_RATE_LEFT        0x2086
+#define TORQUE_RATE_RIGHT       0x2087
+
+#define TARGET_VEL_LEFT         0x2088
+#define TARGET_VEL_RIGHT        0x2089
+#define TARGET_TORQUE_LEFT      0x2090
+#define TARGET_TORQUE_RIGHT     0x2091
+
+#define ACTUAL_POS_HIGH_LEFT    0x20A7
+#define ACTUAL_POS_LOW_LEFT     0x20A8
+#define ACTUAL_POS_HIGH_RIGHT   0x20A9
+#define ACTUAL_POS_LOW_RIGHT    0x20AA
+#define ACTUAL_VEL_LEFT         0x20AB
+#define ACTUAL_VEL_RIGHT        0x20AC
+#define ACTUAL_CURR_LEFT        0x20AD
+#define ACTUAL_CURR_RIGHT       0x20AE
+#define DC_VOLT                 0x20A1
+#define MOT_TEMP                0x20A4
+#define DRIVER_TEMP             0x20B0
+#define ERROR_LEFT              0x20A5
+#define ERROR_RIGHT             0x20A6
